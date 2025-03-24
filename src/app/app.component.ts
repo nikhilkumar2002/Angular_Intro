88448b47da0caa0f228422d587339077213efde5
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-03-24 16:22:43 +0530

    UC2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterModule], // Add RouterModule here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = "Hello from BridgeLabz";
+  logoUrl: string = "https://www.bridgelabz.com/assets/images/BridgeLabz%20Logo.webp"; // Replace with actual URL
 }

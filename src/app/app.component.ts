0ea7a8548cebe5039e832d330964b58d3ae38e6a
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-03-24 15:41:18 +0530

    UC1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule], // Add RouterModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HELLO-WORLD';
+  title: string = "Hello from BridgeLabz";
 }
